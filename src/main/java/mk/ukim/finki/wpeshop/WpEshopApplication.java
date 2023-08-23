d1ee8bba6c902f61eceb3ee5af031f1bb986c87c
Viktorija Trenceska <vikitrenceska@gmail.com>
2023-08-23 18:08:01 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wpeshop/WpEshopApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wpeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpEshopApplication.class, args);
+	}
+
+}
