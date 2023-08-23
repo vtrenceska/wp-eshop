ece765aaa0ed568cee166e5847eb0edcba7cc65b
Viktorija Trenceska <vikitrenceska@gmail.com>
2023-08-23 18:27:00 +0200

    Added HelloWorldServlet

--- src/main/java/mk/ukim/finki/wpeshop/web/HelloWorldServlet.java
+++ src/main/java/mk/ukim/finki/wpeshop/web/HelloWorldServlet.java
@@ -1,4 +1,31 @@
 package mk.ukim.finki.wpeshop.web;
 
-public class HelloWorldServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "hello", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+
+        if(name == null) {
+            name = "";
+        }
+        if(surname == null) {
+            surname = "";
+        }
+
+        PrintWriter writer = resp.getWriter();
+        writer.format("<html><head></head><body><h1>Hi %s %s!</h1></body></html>", name, surname);
+        writer.flush();
+    }
 }
